6325067002973a10cbbbc00fada44483809eb45a
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2025-04-05 02:34:45 +0600

    btm appbar

---
+++ lib/home/bottom_navigationbar.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'home_screen.dart';
+import 'searchbar.dart';
+
+class BottomNavBar extends StatefulWidget {
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _widgetOptions = [
+    HomeScreen(),
+    Search_bar(),
+    Search_bar(),
+    Search_bar(),
+  ];
+
+  void _onTabChange(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: _widgetOptions[_selectedIndex],
+
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(24),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 10,
+              offset: Offset(0, 5),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 12),
+          child: GNav(
+            gap: 8,
+            selectedIndex: _selectedIndex,
+            onTabChange: _onTabChange,
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            backgroundColor: Colors.white,
+            color: Colors.grey[600],
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.black.withOpacity(0.8),
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.menu_open,
+                text: 'Menu',
+              ),
+              GButton(
+                icon: Icons.menu_book,
+                text: 'Courses',
+              ),
+              GButton(
+                icon: Icons.person_outline,
+                text: 'Profile',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
