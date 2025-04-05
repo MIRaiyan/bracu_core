aadba922b6ae1e7b57e5b7209de3a86dc59205d5
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2025-04-05 14:29:25 +0600

    menu

--- lib/menu/menu.dart
+++ lib/menu/menu.dart
@@ -5,19 +5,71 @@ class Menu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> options = [
+      'CGPA Calculator',
+      'Thesis Finder',
+      'AI Assistant',
+      'Trending Courses',
+      'Faculty Information',
+      'Other'
+    ];
+    final List<IconData> icons = [
+      Icons.calculate,
+      Icons.search,
+      Icons.smart_toy,
+      Icons.trending_up,
+      Icons.info,
+      Icons.more_horiz
+    ];
+
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Menu'),
+        backgroundColor: Colors.white,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          itemCount: options.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: 0.8,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                // Handle tap
+              },
+              child: _buildCard(options[index], icons[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard(String option, IconData icon) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+        side: const BorderSide(color: Colors.teal, width: 1),
+      ),
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Menu',
-              style: TextStyle(fontSize: 24),
+          children: [
+            Icon(icon, size: 40, color: Colors.teal),
+            const SizedBox(height: 10),
+            Text(
+              option,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
             ),
-            Text("Its a placeholder for the menu screen"),
           ],
         ),
       ),
