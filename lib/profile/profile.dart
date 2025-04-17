7388321c82352e93ec2d64330dba217596c91965
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2025-04-17 15:26:06 +0600

    salekain
    Co-Authored-By: LordProcastinator <178675786+lordprocastinator@users.noreply.github.com>

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,5 +1,6 @@
 import 'package:bracu_core/auth/login.dart';
 import 'package:bracu_core/menu/ai_assistant.dart';
+import 'package:bracu_core/menu/cgpa_calc.dart';
 import 'package:bracu_core/profile/feedback.dart';
 import 'package:bracu_core/profile/privacy_policy.dart';
 import 'package:flutter/material.dart';
@@ -149,7 +150,7 @@ class Profile extends StatelessWidget {
               _buildListTile(Icons.admin_panel_settings_outlined, 'Admin Panel', Colors.red, context, null),
             _buildListTile(Icons.feedback_outlined, 'AI', Colors.black, context, AIAssistantScreen()),
             _buildListTile(Icons.share_outlined, 'Invite', Colors.black, context, null),
-            _buildListTile(Icons.help_outline, 'Help', Colors.black, context, null),
+            _buildListTile(Icons.help_outline, 'Help', Colors.black, context, CGPACalculatorApp()),
             _buildListTile(Icons.privacy_tip_outlined, 'Privacy Policy', Colors.black, context, PrivacyPolicyScreen()),
             _buildListTile(Icons.logout_outlined, 'Logout', Colors.red, context, null),
           ],
